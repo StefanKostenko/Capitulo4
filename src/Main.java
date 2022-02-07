06ca79f46bc1adc5472b3a593d75341a2bd72e5a
alu11395193 <kostenkostephan@gmail.com>
2022-02-07 12:43:30 +0100

    Refactorizar 1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //Pedir variable
+
         Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
 
