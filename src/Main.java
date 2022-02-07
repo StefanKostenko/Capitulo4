f7b9e807b8a5fe3df64df338d24115ee7538ee49
alu11395193 <kostenkostephan@gmail.com>
2022-02-07 12:40:14 +0100

    Original

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
+
         int dato=teclado.nextInt();
         int vector[]=new int[dato];
+
         System.out.println("\nVector inicial hasta :"+dato);
         for (int i = 0; i < vector.length; i++) {
             if (i%10==0) System.out.println();
